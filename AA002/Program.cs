da4e01cebb6ab5e89451ab076c76e5a4808a8cf6
Artem <a.khimin@yandex.ru>
2021-08-20 19:04:41 +0300

    Последовательный и параллельный вызов асинхронных операций

---
+++ AA002/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA002
+{
+    class Program
+    {
+        static void Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.Write($"Факториал равен {result} ");
+        }
+
+        static int FactorialInt(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        // определение асинхронного метода
+        static async void FactorialVoidAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+            Console.WriteLine(" - FactorialVoidAsync");
+        }
+
+        static async Task FactorialTaskAsync(int n)  
+        {
+            await Task.Run(() => Factorial(n));
+            Console.WriteLine(" - FactorialTaskAsync");
+        }
+
+        static async Task<int> FactorialTaskTAsync(int n)    //Task - класс
+        {
+            return await Task.Run(() => FactorialInt(n));
+            Console.WriteLine(" - FactorialTaskTAsync");
+        }
+
+        static async ValueTask<int> FactorialValueTaskAsync(int n)        //ValueTask - структура
+        {
+            return await Task.Run(() => FactorialInt(n));
+        }
+
+        static async Task Main(string[] args)
+        {
+            //Возвращение результата из асинхронного метода
+
+            Console.WriteLine("Введите значение для расчета факториала: ");
+
+            int f =Convert.ToInt32( Console.ReadLine());
+
+            FactorialVoidAsync(f);
+
+            FactorialTaskAsync(f);
+
+            int fTaskT = await FactorialTaskTAsync(f);
+            Console.WriteLine(fTaskT);
+
+            int fValueTask = await FactorialValueTaskAsync(f);
+            Console.WriteLine(fValueTask);
+
+        }
+    }
+}
---
+++ AA003/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA003
+{
+    class Program
+    {
+        //Последовательный и параллельный вызов асинхронных операций
+
+        static void Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Факториал числа {n} равен {result}");
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync()
+        {
+            await Task.Run(() => Factorial(4));
+            await Task.Run(() => Factorial(3));
+            await Task.Run(() => Factorial(5));
+        }
+
+        static async void FactorialWhenAllAsync()
+        {
+            Task t1 = Task.Run(() => Factorial(9));
+            Task t2 = Task.Run(() => Factorial(8));
+            Task t3 = Task.Run(() => Factorial(7));
+            await Task.WhenAll(new[] { t1, t2, t3 });
+        }
+
+        static void Main(string[] args)
+        {
+            FactorialAsync();
+
+            FactorialWhenAllAsync();
+            Console.Read();
+        }
+    }
+}
