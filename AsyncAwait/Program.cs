630d9f845e4679d41dad2c8612614085bcc4f2f0
Artem <a.khimin@yandex.ru>
2021-08-20 15:15:00 +0300

    тест асинхронных операций

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait
 {
     class Program
     {
+        static void Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Thread.Sleep(5000);
+            Console.WriteLine($"Факториал равен {result}");
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+
+
+        static void Factorial()
+        {
+            int result = 1;
+            for (int i = 1; i <= 6; i++)
+            {
+                result *= i;
+            }
+            Thread.Sleep(8000);
+            Console.WriteLine($"Факториал равен {result}");
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync()
+        {
+            Console.WriteLine("Начало метода FactorialAsync"); // выполняется синхронно
+            await Task.Run(() => Factorial());                // выполняется асинхронно
+            Console.WriteLine("Конец метода FactorialAsync");
+        }
+
+        static async void ReadWriteAsync()
+        {
+            string s = "Hello world! One step at a time";
+
+            // hello.txt - файл, который будет записываться и считываться
+            using (StreamWriter writer = new StreamWriter("hello.txt", false))
+            {
+                await writer.WriteLineAsync(s);  // асинхронная запись в файл
+            }
+            using (StreamReader reader = new StreamReader("hello.txt"))
+            {
+                string result = await reader.ReadToEndAsync();  // асинхронное чтение из файла
+                Console.WriteLine(result);
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            FactorialAsync();   // вызов асинхронного метода
+
+            Console.WriteLine("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Квадрат числа равен {n * n}");
+
+            Console.Read();
+
+            ReadWriteAsync();
+
+            Console.WriteLine("Некоторая работа");
+            Console.Read();
+
+            Console.WriteLine("*********************");
+
+            FactorialAsync(5);
+            FactorialAsync(6);
+            Console.WriteLine("Некоторая работа,  FactorialAsync c параметром");
+            Console.Read();
+
+
         }
+
     }
 }
