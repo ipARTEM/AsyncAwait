44b56dd47848ecee25bb27e69aa7bdb731c4cb6e
Artem <a.khimin@yandex.ru>
2021-08-21 14:29:01 +0300

    тест Asynchronous Streams

---
+++ AA004/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AA004
+{
+    class Program
+    {
+        static void Factorial(int n)
+        {
+            if (n < 1)
+                throw new Exception($"{n} : число не должно быть меньше 1");
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Факториал числа {n} равен {result}");
+        }
+
+        static async void FactorialAsync(int n)
+        {
+            try
+            {
+                await Task.Run(() => Factorial(n));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async void FactorialAsync2(int n)
+        {
+            Task task = null;
+            try
+            {
+                task = Task.Run(() => Factorial(n));
+                await task;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(task.Exception.InnerException.Message);
+                Console.WriteLine($"FactorialAsync2 - IsFaulted: {task.IsFaulted}");
+            }
+        }
+
+        static async Task DoMultipleAsync()
+        {
+            Task allTasks = null;
+
+            try
+            {
+                Task t1 = Task.Run(() => Factorial(-3));
+                Task t2 = Task.Run(() => Factorial(-5));
+                Task t3 = Task.Run(() => Factorial(-10));
+
+                allTasks = Task.WhenAll(t1, t2, t3);
+                await allTasks;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DoMultipleAsync - Исключение: " + ex.Message);
+                Console.WriteLine("DoMultipleAsync - IsFaulted: " + allTasks.IsFaulted);
+                foreach (var inx in allTasks.Exception.InnerExceptions)
+                {
+                    Console.WriteLine("DoMultipleAsync - Внутреннее исключение: " + inx.Message);
+                }
+            }
+        }
+
+        static async void FactorialFinallyAsync(int n)
+        {
+            try
+            {
+                await Task.Run(() => Factorial(n)); ;
+            }
+            catch (Exception ex)
+            {
+                await Task.Run(() => Console.WriteLine(ex.Message));
+            }
+            finally
+            {
+                await Task.Run(() => Console.WriteLine("FactorialFinallyAsync - await в блоке finally"));
+            }
+        }
+
+        static void FactorialIsCancellationRequested(int n, CancellationToken token)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+                result *= i;
+                Console.WriteLine($"Факториал числа {i} равен {result}");
+                Thread.Sleep(1000);
+            }
+        }
+        // определение асинхронного метода
+        static async void FactorialIsCancellationRequestedAsync(int n, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                return;
+            await Task.Run(() => FactorialIsCancellationRequested(n, token));
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            FactorialAsync(-4);
+            FactorialAsync(6);
+
+            FactorialAsync2(-4);
+            FactorialAsync2(6);
+
+            DoMultipleAsync();
+
+            FactorialFinallyAsync(-5);
+
+            Console.WriteLine("*******************");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            FactorialIsCancellationRequestedAsync(6, token);
+            Thread.Sleep(3000);
+            cts.Cancel();
+            Console.Read();
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ AA005/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AA005
+{
+    //Асинхронные стримы
+    //Asynchronous Streams
+
+    //public interface IAsyncEnumerable<out T>
+    //{
+    //    IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default);
+    //}
+
+    public interface IAsyncEnumerator<out T> : IAsyncDisposable
+    {
+        T Current { get; }
+        ValueTask<bool> MoveNextAsync();
+    }
+    public interface IAsyncDisposable
+    {
+        ValueTask DisposeAsync();
+    }
+
+
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    class Program
+    {
+        public static async IAsyncEnumerable<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product{Name="Car", Description="A car (or automobile) is a wheeled motor vehicle used for transportation."},
+                new Product{Name="Bicycle", Description="A bicycle, also called a bike or cycle, is a human-powered or motor-powered, pedal-driven, single-track vehicle, having two wheels attached to a frame, one behind the other."},
+                new Product{Name="Airplane", Description="An airplane or aeroplane (informally plane) is a fixed-wing aircraft that is propelled forward by thrust from a jet engine, propeller, or rocket engine."},
+                new Product{Name="Ship", Description="A ship is a large watercraft that travels the world's oceans and other sufficiently deep waterways, carrying goods or passengers, or in support of specialized missions, such as defense, research, and fishing."},
+                new Product{Name="Rocket", Description="A rocket  is a projectile that spacecraft, aircraft or other vehicles use to obtain thrust from a rocket engine. "},
+
+            };
+
+            foreach (Product product in products)
+            {
+                await Task.Delay(1000);
+                yield return product;
+            }
+
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            await foreach (var product in GetProducts())
+            {
+                Console.WriteLine($"Name: {product.Name}, Description: {product.Description} ");
+            }
+
+            await foreach (var number in GetNumbersAsync())
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static async IAsyncEnumerable<int> GetNumbersAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+    }
+
+    class Repository
+    {
+        string[] data = { "Tom", "Sam", "Kate", "Alice", "Bob" };
+        public async IAsyncEnumerable<string> GetDataAsync()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine($"Получаем {i + 1} элемент");
+                await Task.Delay(500);
+                yield return data[i];
+            }
+        }
+    }
+}
